1ce6f02ec8c37c928d474bf13b648c6ce6af0a5d
Fedex159 <fede.gallina94@gmail.com>
2021-12-12 17:46:41 -0300

    whiteboard component created

---
+++ client/src/components/Whiteboard/Whiteboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./Whiteboard.module.css";
+
+function Whiteboard() {
+  return <div className={s.container}>Whiteboard</div>;
+}
+
+export default Whiteboard;
