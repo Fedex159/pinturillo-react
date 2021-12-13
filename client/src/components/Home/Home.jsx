fc58278f9202e3a51490090cb86c8e8cdeae98c7
Fedex159 <fede.gallina94@gmail.com>
2021-12-13 16:05:33 -0300

    load socket in home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,12 +3,14 @@ import Users from "../Users/Users";
 import Whiteboard from "../Whiteboard/Whiteboard";
 import Chat from "../Chat/Chat";
 import s from "./Home.module.css";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3001/");
 
 function Home() {
   return (
     <div className={s.container}>
       <Users />
-      <Whiteboard />
+      <Whiteboard socket={socket} />
       <Chat />
     </div>
   );
