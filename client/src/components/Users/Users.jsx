7afb844f9784266b82fb0232f98c0c182dca54f7
Fedex159 <fede.gallina94@gmail.com>
2021-12-14 16:38:01 -0300

    initial logical for users

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import s from "./Users.module.css";
 
-function Users() {
-  return <div className={s.container}>Users</div>;
+function Users({ socket }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      axios
+        .get("http://localhost:3001/rooms/61b8e426d5c7eb84302b152e")
+        .then((response) => response.data)
+        .then((data) => setUsers(data.users));
+    });
+
+    socket.on("user connected", (id) => {
+      setUsers((prev) => [...prev, id]);
+    });
+
+    socket.on("user disconnected", (id) => {
+      setUsers((prev) => {
+        return prev.filter((u) => u !== id);
+      });
+    });
+  }, [socket]);
+
+  return (
+    <div className={s.container}>
+      Users
+      {users.map((user) => (
+        <h2>{user}</h2>
+      ))}
+    </div>
+  );
 }
 
 export default Users;
