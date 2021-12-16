21c444c1cf5d6e7cd900c0978bba5a77116ca2ae
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 14:51:57 -0300

    load navbar

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NavBar from "./NavBar/NavBar";
 import Name from "./Name/Name";
 import s from "./Home.module.css";
 
 function Home() {
   return (
     <div className={s.container}>
+      <NavBar />
       <Name />
     </div>
   );
