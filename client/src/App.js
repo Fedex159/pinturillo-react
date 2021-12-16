951c4d728a199d36becdabef054dc1bb0d53153b
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 12:04:20 -0300

    route for home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 import Game from "./components/Game/Game";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/game" element={<Game />} />
       </Routes>
     </div>
