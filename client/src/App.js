cfb57924d4f30f1a857aa02fb583d9b6f04ccc86
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 11:10:54 -0300

    initial config for redux and router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./state/store/index";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
