3c2100e67027174240509b408a3249c9bc87b2be
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 12:04:42 -0300

    component for select name

---
+++ client/src/components/Home/Name/Name.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setName } from "../../../state/actions/index";
+import s from "./Name.module.css";
+
+function Name() {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const value = event.target.name.value;
+
+    if (value) {
+      dispatch(setName(value));
+      event.target.reset();
+    }
+  };
+
+  return (
+    <div className={s.container}>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nick:
+          <input name="name" placeholder="Name..." />
+        </label>
+        <button type="submit">Play</button>
+      </form>
+    </div>
+  );
+}
+
+export default Name;
