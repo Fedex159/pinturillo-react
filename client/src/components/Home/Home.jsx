f006e063504730378cf5b7e948cfe9f4f235877c
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 15:12:25 -0300

    state for options

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar/NavBar";
 import Name from "./Name/Name";
+import CreateRoom from "./CreateRoom/CreateRoom";
 import s from "./Home.module.css";
 
+const options = {
+  name: <Name />,
+  create: <CreateRoom />,
+};
+
 function Home() {
+  const [option, setOption] = useState("name");
+
   return (
     <div className={s.container}>
-      <NavBar />
-      <Name />
+      <NavBar setOption={setOption} />
+      {options[option]}
     </div>
   );
 }
