b0b4185a46cd3db3ac6f681d390dba5d708fc69e
Fedex159 <fede.gallina94@gmail.com>
2021-12-16 15:48:40 -0300

    create room add function

--- client/src/components/Home/CreateRoom/CreateRoom.jsx
+++ client/src/components/Home/CreateRoom/CreateRoom.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { createRoom } from "../../../utils";
+import { useNavigate } from "react-router-dom";
 import s from "./CreateRoom.module.css";
 
 function CreateRoom() {
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    event.target.reset();
+    const title = event.target.title.value;
+    const password = event.target.password.value;
+
+    if (title && password) {
+      createRoom({ title, password })
+        .then((data) => {
+          event.target.reset();
+          navigate(`/game/${data.message}`);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
     <div className={s.container}>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Title..." />
-        <input type="password" name="password" placeholder="Password..." />
+        <input type="text" name="title" placeholder="Title*" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password*"
+          required
+        />
         <button type="submit">Create</button>
       </form>
     </div>
