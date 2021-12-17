149a2d91b0fd3a3232bca289ad0827bb3492e188
Fedex159 <fede.gallina94@gmail.com>
2021-12-17 12:05:32 -0300

    socket only when component load

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Users from "../Users/Users";
 import Whiteboard from "../Whiteboard/Whiteboard";
 import Chat from "../Chat/Chat";
+import { useParams } from "react-router-dom";
 import s from "./Game.module.css";
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3001/");
 
 function Game() {
+  const { id } = useParams();
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:3001/"));
+  }, []);
+
   return (
     <div className={s.container}>
-      <Users socket={socket} />
-      <Whiteboard socket={socket} />
-      <Chat socket={socket} />
+      <Users socket={socket} id={id} />
+      <Whiteboard socket={socket} id={id} />
+      <Chat socket={socket} id={id} />
     </div>
   );
 }
