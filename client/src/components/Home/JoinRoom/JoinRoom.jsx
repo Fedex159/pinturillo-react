40b5403414fa7914e5210f7173dbe2c740082b32
Fedex159 <fede.gallina94@gmail.com>
2021-12-19 18:24:49 -0300

    add message to join room

--- client/src/components/Home/JoinRoom/JoinRoom.jsx
+++ client/src/components/Home/JoinRoom/JoinRoom.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FormContainer from "../FormContainer/FormContainer";
-import s from "./JoinRoom.module.css";
+import Message from "../Message/Message";
+import { joinRoom } from "../../../utils";
+import { setAccess } from "../../../state/actions";
+// import s from "./JoinRoom.module.css";
 
 function JoinRoom() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.name);
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const id = event.target.id.value;
     const password = event.target.password.value;
-    if (id) {
-      axios
-        .get(`http://localhost:3001/rooms?id=${id}&password=${password}`)
-        .then((response) => response.data)
+
+    if (id && name) {
+      joinRoom(id, password)
         .then((data) => {
           if (data.message) {
+            dispatch(setAccess(true));
             navigate({ pathname: `/game/${id}` }, { replace: true });
+          } else {
+            setMessage("Id or password is incorrect!!!");
+            setShowMessage(true);
           }
-        });
-      event.target.reset();
+        })
+        .catch((err) => console.log(err));
+    } else if (id) {
+      setMessage("Name is required!!!");
+      setShowMessage(true);
     }
   };
 
@@ -32,6 +45,9 @@ function JoinRoom() {
         <input type="password" name="password" placeholder="Password" />
         <button type="submit">Join</button>
       </form>
+      <Message show={showMessage} error={true}>
+        <span>{message}</span>
+      </Message>
     </FormContainer>
   );
 }
