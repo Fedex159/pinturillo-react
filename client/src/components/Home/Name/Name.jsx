db4af2c9bc8903036ed02dc95ef989f08ae68834
Fedex159 <fede.gallina94@gmail.com>
2021-12-19 14:53:27 -0300

    add title and form container

--- client/src/components/Home/Name/Name.jsx
+++ client/src/components/Home/Name/Name.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setName } from "../../../state/actions/index";
+import FormContainer from "../FormContainer/FormContainer";
 import s from "./Name.module.css";
 
 function Name() {
@@ -17,15 +18,14 @@ function Name() {
   };
 
   return (
-    <div className={s.container}>
+    <FormContainer>
+      <h2>Change Name</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Nick:
-          <input name="name" placeholder="Name..." />
-        </label>
+        <label>Name</label>
+        <input name="name" />
         <button type="submit">Save</button>
       </form>
-    </div>
+    </FormContainer>
   );
 }
 
