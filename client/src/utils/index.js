40573267bcbdf130251acb2ba41267b4db080bab
Fedex159 <fede.gallina94@gmail.com>
2021-12-19 18:19:05 -0300

    add utils for join room

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,3 +4,10 @@ export const createRoom = async (data) => {
   const response = await axios.post("http://localhost:3001/rooms", data);
   return response.data;
 };
+
+export const joinRoom = async (id, password) => {
+  const response = await axios.get(
+    `http://localhost:3001/rooms?id=${id}&password=${password}`
+  );
+  return response.data;
+};
