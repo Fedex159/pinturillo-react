73afe794056029f7617b717b4b1d7bb957a1cd99
Fedex159 <fede.gallina94@gmail.com>
2021-12-19 14:52:08 -0300

    add join room component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import NavBar from "./NavBar/NavBar";
 import Name from "./Name/Name";
 import CreateRoom from "./CreateRoom/CreateRoom";
+import JoinRoom from "./JoinRoom/JoinRoom";
 import s from "./Home.module.css";
 
 const options = {
   name: <Name />,
   create: <CreateRoom />,
+  join: <JoinRoom />,
 };
 
 function Home() {
