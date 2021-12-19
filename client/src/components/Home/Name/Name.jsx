e6920ec4bc7411c9021f5415dd558fe78b2faa7c
Fedex159 <fede.gallina94@gmail.com>
2021-12-19 17:54:53 -0300

    add name message when save

--- client/src/components/Home/Name/Name.jsx
+++ client/src/components/Home/Name/Name.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setName } from "../../../state/actions/index";
 import FormContainer from "../FormContainer/FormContainer";
-import s from "./Name.module.css";
+import Message from "../Message/Message";
+// import s from "./Name.module.css";
 
 function Name() {
   const dispatch = useDispatch();
+  const name = useSelector((state) => state.name);
+  const [showMessage, setShowMessage] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,7 +16,7 @@ function Name() {
 
     if (value) {
       dispatch(setName(value));
-      event.target.reset();
+      setShowMessage(true);
     }
   };
 
@@ -22,9 +25,12 @@ function Name() {
       <h2>Change Name</h2>
       <form onSubmit={handleSubmit}>
         <label>Name</label>
-        <input name="name" />
+        <input name="name" defaultValue={name} />
         <button type="submit">Save</button>
       </form>
+      <Message show={showMessage} error={false}>
+        <span>Name saved!!!</span>
+      </Message>
     </FormContainer>
   );
 }
