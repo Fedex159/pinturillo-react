1977b6339da8da2df10d0751576714a75b87cda0
Fedex159 <fede.gallina94@gmail.com>
2021-12-20 15:58:21 -0300

    config env url for socket connection

--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -5,13 +5,17 @@ import Chat from "../Chat/Chat";
 import { useParams } from "react-router-dom";
 import s from "./Game.module.css";
 import { io } from "socket.io-client";
+import dotenv from "dotenv";
+dotenv.config();
+
+const URL_CONNECTION = process.env.REACT_APP_API || "http://localhost:3001";
 
 function Game() {
   const { id } = useParams();
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:3001/"));
+    setSocket(io(URL_CONNECTION));
   }, []);
 
   return (
