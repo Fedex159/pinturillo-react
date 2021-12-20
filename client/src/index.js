40cea737f38dc8924c3de72cb66df9ac683e6dd8
Fedex159 <fede.gallina94@gmail.com>
2021-12-20 15:58:05 -0300

    config url base for axios

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./state/store/index";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 export const createRoom = async (data) => {
-  const response = await axios.post("http://localhost:3001/rooms", data);
+  const response = await axios.post("/rooms", data);
   return response.data;
 };
 
 export const joinRoom = async (id, password) => {
-  const response = await axios.get(
-    `http://localhost:3001/rooms?id=${id}&password=${password}`
-  );
+  const response = await axios.get(`/rooms?id=${id}&password=${password}`);
   return response.data;
 };
 
 export const getUsersRoom = async (id) => {
-  const response = await axios.get(`http://localhost:3001/rooms/users/${id}`);
+  const response = await axios.get(`/rooms/users/${id}`);
   return response.data;
 };
