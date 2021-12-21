fe49cae13836831c8fe9d0ea38227fbc4e5676ec
Fedex159 <fede.gallina94@gmail.com>
2021-12-21 11:58:25 -0300

    add usercard and more info in sockets

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -2,12 +2,40 @@ import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getUsersRoom } from "../../utils";
+import pencil from "../../assets/imgs/pencil.png";
 import s from "./Users.module.css";
+
+function UserCard({ index, name, points }) {
+  return (
+    <div className={s.cardUser}>
+      <span>{index + 1}</span>
+      <div className={s.userInfo}>
+        <h2>{points}</h2>
+        <h2>{name}</h2>
+      </div>
+      <img src={pencil} alt="pencil" />
+    </div>
+  );
+}
 
 function Users({ socket, id }) {
   const access = useSelector((state) => state.access);
+  const name = useSelector((state) => state.name);
   const [users, setUsers] = useState([]);
   const [redirect, setRedirect] = useState(false);
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    if (connected) {
+      getUsersRoom(id)
+        .then((data) => {
+          if (data) {
+            setUsers(data);
+          }
+        })
+        .catch((err) => console.log("Error getUsersRoom", err));
+    }
+  }, [id, connected]);
 
   useEffect(() => {
     if (socket) {
@@ -16,37 +44,33 @@ function Users({ socket, id }) {
         setRedirect(true);
       }
       socket.on("connect", () => {
-        socket.emit("room", id);
-        socket.emit("user connected", socket.id, id);
-        getUsersRoom(id).then((data) => {
-          if (data) {
-            setUsers(data);
-            return;
-          }
-        });
+        socket.emit("room", id, name);
+        socket.emit("user connected", socket.id, id, name);
+        setConnected(true);
       });
 
-      socket.on("user connected", (userId) => {
-        setUsers((prev) => [...prev, userId]);
+      socket.on("user connected", (user) => {
+        setUsers((prev) => [...prev, user]);
       });
 
-      socket.on("message", (data) => {
-        console.log(data);
-      });
-
-      socket.on("user disconnected", (id) => {
+      socket.on("user disconnected", (userId) => {
         setUsers((prev) => {
-          return prev.filter((u) => u !== id);
+          return prev.filter((u) => u._id !== userId);
         });
       });
     }
-  }, [socket, id, access]);
+  }, [socket, id, access, name]);
 
   return (
     <div className={s.container}>
-      Users
-      {users.map((user) => (
-        <h2 key={user}>{user}</h2>
+      <h4>Users</h4>
+      {users.map((user, index) => (
+        <UserCard
+          key={`${user._id}_${user.name}`}
+          name={user.name}
+          points={user.points}
+          index={index}
+        ></UserCard>
       ))}
       {redirect ? <Navigate to="/" replace={true} /> : null}
     </div>
