b33071139a4e921b73df11d407017774ac3e3053
Fedex159 <fede.gallina94@gmail.com>
2021-12-21 13:47:27 -0300

    styles for chat and name socket

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import s from "./Chat.module.css";
 
 function Chat({ socket, id }) {
+  const name = useSelector((state) => state.name);
+  const refList = useRef(null);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
+    if (refList.current) {
+      refList.current.scrollTo(0, refList.current.scrollHeight);
+    }
+  }, [messages]);
+
+  useEffect(() => {
     if (socket) {
-      socket.on("message incoming", (userId, text) => {
-        setMessages((prev) => [...prev, { userId, text }]);
+      socket.on("message incoming", (name, text, userId) => {
+        setMessages((prev) => [...prev, { name, text, userId }]);
       });
     }
   }, [socket]);
@@ -16,23 +25,26 @@ function Chat({ socket, id }) {
     event.preventDefault();
     const value = event.target.message.value;
     if (value) {
-      socket.emit("message send", socket.id, value, id);
-      setMessages((prev) => [...prev, { userId: socket.id, text: value }]);
+      socket.emit("message send", name, value, id, socket.id);
+      setMessages((prev) => [
+        ...prev,
+        { name: name, text: value, userId: socket.id },
+      ]);
       event.target.reset();
     }
   };
 
   return (
     <div className={s.container}>
-      Chat
-      <ul>
+      <h4>Chat</h4>
+      <ul ref={refList}>
         {messages.map((m, i) => (
           <li key={`${m.userId}_${i}`}>
             <p>
               <span
                 className={socket.id === m.userId ? s.myMessage : s.userMessage}
               >
-                {m.userId}:{" "}
+                {m.name}:{" "}
               </span>
               {m.text}
             </p>
